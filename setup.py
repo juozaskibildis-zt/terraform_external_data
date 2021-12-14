1d7ccf83a3faa130df2362feffa15d356048653c
Juozas <juozas.kibildis@zenitech.co.uk>
2021-12-14 12:09:39 +0200

    Update build information

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "terraform_external_data"},
+    packages=setuptools.find_packages(where="terraform_external_data"),
     python_requires=">=3.6",
 )
